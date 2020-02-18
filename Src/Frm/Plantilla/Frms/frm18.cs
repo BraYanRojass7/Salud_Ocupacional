e9ab65fe6e89d934fd71d578c7c029fdb5099890
Brian Steven Galviz Rojas <briansgalvisrojas@gmail.com>
2020-02-18 17:55:59 -0500

    Menu 100% completado

---
+++ Src/Frm/Plantilla/Frms/frm18.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App_SG_SST_V1_5.Src.Frm.Plantilla.Frms
+{
+    public partial class frm18 : Form
+    {
+        public frm18()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Src/Frm/frm_menu.cs
+++ Src/Frm/frm_menu.cs
@@ -136,7 +136,7 @@ namespace App_SG_SST_V1_5.Src.Frm
                     cmb_est.Items.Add("Seleccionar");
                     cmb_est.Items.Add("4. Objetivos del Sistema...");
                     cmb_est.Items.Add("5. Evaluación inicial ...");
-                    cmb_est.Items.Add("6. Conservación ...");
+                    cmb_est.Items.Add("7. Conservación ...");
                     cmb_est.Items.Add("8. Rendición de cuentas ...");
                     cmb_est.Items.Add("9. Normatividadnacional ...");
                     cmb_est.Items.Add("10. Comunicación ...");
@@ -258,17 +258,194 @@ namespace App_SG_SST_V1_5.Src.Frm
                     txt_desc_est.Enabled = true;
                     //----//
                     break;
-
                 case "4. Objetivos del Sistema...":
-
                     //----//
                     txt_desc_est.Clear();
                     //----//
-                    txt_desc_est.Text = "Objetivos del Sistema de Gestión de la Seguridad y la Salud en el Trabajo SG-SST (1%)";
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\4Objetivos\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
                     //----//
                     txt_desc_est.Enabled = true;
                     //----//
                     break;
+                case "5. Evaluación inicial ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\5Evaluación\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "7. Conservación ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\7Conservación\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "8. Rendición de cuentas ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\8Rendición\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "9. Normatividadnacional ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\9Normatividad\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "10. Comunicación ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\10Comunicación\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "11. Adquisiciones ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\11Adquisiciones\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "12. Contratación ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\12Contratación\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "13. Gestión ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_INTEGRAL_DEL_SG-SST_(15%)\13Gestión\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "14. Condiciones ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTIÓN_DE_LA_SALUD(20%)\14.Condiciones\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "15. Registro ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTIÓN_DE_LA_SALUD(20%)\15.Registro\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "16. Mecanismos ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTIÓN_DE_LA_SALUD(20%)\16.Mecanismos\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "17. Identificación ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTIÓN_DE_PELIGROS_Y_RIESGOS(30%)\17.Identificacion\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "18. Medidas ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTIÓN_DE_PELIGROS_Y_RIESGOS(30%)\18.Medidas\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "19. Plan de prevención ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\GESTION_DE_AMENAZAS(10%)\19.Plan\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "20. Gestión y resultado ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\VERIFICACIÓN_DEL_SG-SST(5%)\20.Gestión\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+                case "21. Acciones ...":
+                    //----//
+                    txt_desc_est.Clear();
+                    //----//
+                    ruta = @"\\192.168.1.180\informes\TIC'S\Src\Ciclo\VERIFICACIÓN_DEL_SG-SST(5%)\20.Gestión\Descripcion.txt";
+                    //----//
+                    Leer_texto(ruta);
+                    //----//
+                    txt_desc_est.Enabled = true;
+                    //----//
+                    break;
+
             }
         }
 
@@ -312,7 +489,6 @@ namespace App_SG_SST_V1_5.Src.Frm
                     break;
 
                 case "1. Recursos Financieros...":
-
                     //----//
                     Plantilla.Frms.frm_1 FRMS = new Plantilla.Frms.frm_1();
                     //----//
@@ -321,16 +497,134 @@ namespace App_SG_SST_V1_5.Src.Frm
                     break;
 
                 case "2. Capacitación en el Sistema...":
-
                     //----//
                     Plantilla.Frms.frm2 FRMS2 = new Plantilla.Frms.frm2();
                     //----//
                     FRMS2.Show();
                     //----//
                     break;
+                case "4. Objetivos del Sistema...":
+                    //----//
+                    Plantilla.Frms.frm4 FRMS4 = new Plantilla.Frms.frm4();
+                    //----//
+                    FRMS4.Show();
+                    //----//
+                    break;
+                case "5. Evaluación inicial ...":
+                    //----//
+                    Plantilla.Frms.frm5 FRMS5 = new Plantilla.Frms.frm5();
+                    //----//
+                    FRMS5.Show();
+                    //----//
+                    break;
+                case "7. Conservación ...":
+                    //----//
+                    Plantilla.Frms.frm7 FRMS7 = new Plantilla.Frms.frm7();
+                    //----//
+                    FRMS7.Show();
+                    //----//
+                    break;
+                case "8. Rendición de cuentas ...":
+                    //----//
+                    Plantilla.Frms.frm8 FRMS8 = new Plantilla.Frms.frm8();
+                    //----//
+                    FRMS8.Show();
+                    //----//
+                    break;
+                case "9. Normatividadnacional ...":
+                    //----//
+                    Plantilla.Frms.frm9 FRMS9 = new Plantilla.Frms.frm9();
+                    //----//
+                    FRMS9.Show();
+                    //----//
+                    break;
+                case "10. Comunicación ...":
+                    //----//
+                    Plantilla.Frms.frm10 FRMS10 = new Plantilla.Frms.frm10();
+                    //----//
+                    FRMS10.Show();
+                    //----//
+                    break;
+                case "11. Adquisiciones ...":
+                    //----//
+                    Plantilla.Frms.frm11 FRMS11 = new Plantilla.Frms.frm11();
+                    //----//
+                    FRMS11.Show();
+                    //----//
+                    break;
+                case "12. Contratación ...":
+                    //----//
+                    Plantilla.Frms.frm12 FRMS12 = new Plantilla.Frms.frm12();
+                    //----//
+                    FRMS12.Show();
+                    //----//
+                    break;
+                case "13. Gestión ...":
+                    //----//
+                    Plantilla.Frms.frm13 FRMS13 = new Plantilla.Frms.frm13();
+                    //----//
+                    FRMS13.Show();
+                    //----//
+                    break;
+                case "14. Condiciones ...":
+                    //----//
+                    Plantilla.Frms.frm14 FRMS14 = new Plantilla.Frms.frm14();
+                    //----//
+                    FRMS14.Show();
+                    //----//
+                    break;
+                case "15. Registro ...":
+                    //----//
+                    Plantilla.Frms.frm15 FRMS15 = new Plantilla.Frms.frm15();
+                    //----//
+                    FRMS15.Show();
+                    //----//
+                    break;
+                case "16. Mecanismos ...":
+                    //----//
+                    Plantilla.Frms.frm16 FRMS16 = new Plantilla.Frms.frm16();
+                    //----//
+                    FRMS16.Show();
+                    //----//
+                    break;
+                case "17. Identificación ...":
+                    //----//
+                    Plantilla.Frms.frm17 FRMS17 = new Plantilla.Frms.frm17();
+                    //----//
+                    FRMS17.Show();
+                    //----//
+                    break;
+                case "18. Medidas ...":
+                    //----//
+                    Plantilla.Frms.frm18 FRMS18 = new Plantilla.Frms.frm18();
+                    //----//
+                    FRMS18.Show();
+                    //----//
+                    break;
+                case "19. Plan de prevención ...":
+                    //----//
+                    Plantilla.Frms.frm19 FRMS19 = new Plantilla.Frms.frm19();
+                    //----//
+                    FRMS19.Show();
+                    //----//
+                    break;
+                case "20. Gestión y resultado ...":
+                    //----//
+                    Plantilla.Frms.frm20 FRMS20 = new Plantilla.Frms.frm20();
+                    //----//
+                    FRMS20.Show();
+                    //----//
+                    break;
+                case "21. Acciones ...":
+                    //----//
+                    Plantilla.Frms.frm21 FRMS21 = new Plantilla.Frms.frm21();
+                    //----//
+                    FRMS21.Show();
+                    //----//
+                    break;
+
 
                 default:
-
                     //----//
                     MessageBox.Show("Debe seleccionar un estandar primero", "SELECCION", MessageBoxButtons.OK, MessageBoxIcon.Error);
                     break;
